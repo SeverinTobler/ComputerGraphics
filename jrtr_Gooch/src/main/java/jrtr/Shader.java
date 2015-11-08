49087cf532129383a4720ab802999999c1f565d1
Severin Tobler <tobler.severin@gmail.com>
2015-11-08 12:01:08 +0100

    clean up

--- jrtr_Gooch/src/main/java/jrtr/Shader.java
+++
@@ -1,9 +0,0 @@
-package jrtr;
-
-/**
- * Declares the functionality to manage shaders.
- */
-public interface Shader {
-
-	public void load(String vertexFileName, String fragmentFileName) throws Exception;
-}
